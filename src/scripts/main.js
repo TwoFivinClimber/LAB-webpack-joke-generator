9f4a8c1da6475684f2b27252328af7c7e835d772
Brett Hughes <twofivinclimber@gmail.com>
2022-05-21 08:17:11 -0500

    working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,21 +2,29 @@
 
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
-import jokeData from '../api/jokeData';
+import getJoke from '../api/jokeData';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `<button id="joke-btn" type="button" class="joke-btn btn btn-primary" >Get a Joke</button><button id="punchLine" type="button" style='display: none' class="punch-btn btn btn-primary" >Punch Line ?</button>
+  document.querySelector('#app').innerHTML = `<h1>Parse over and Get Your Jokes</h1><button id="joke-btn" type="button" class="joke-btn btn btn-primary" >Get a Joke</button>
+  <button id="punch-btn" type="button" style='display: none' class="punch-btn btn btn-primary" >Get Punch Line</button><div id="jokeDiv"></div>
   `;
-
-  jokeData().then(console.warn);
 };
 
 const jokeEvent = () => {
   document.querySelector('#joke-btn').addEventListener('click', () => {
-    jokeData().then((response) => {
-      document.querySelector('#jokeDiv').innerHTML = response.setup;
-      document.querySelector('#joke-btn').style.display = 'none';
-      document.querySelector('#punchLine').style.display = 'block';
+    const jokeDiv = document.querySelector('#jokeDiv');
+    const jokeBtn = document.querySelector('#joke-btn');
+    const punchBtn = document.querySelector('#punch-btn');
+    getJoke().then((response) => {
+      jokeDiv.innerHTML = response.setup;
+      jokeBtn.style.display = 'none';
+      punchBtn.style.display = 'block';
+      punchBtn.addEventListener('click', () => {
+        jokeDiv.innerHTML = response.delivery;
+        punchBtn.style.display = 'none';
+        jokeBtn.style.display = 'block';
+        jokeBtn.innerHTML = 'Get Another';
+      });
     });
   });
 };
