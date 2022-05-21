922a8e9298a98ca969561d07882a03219c6693cc
Brett Hughes <twofivinclimber@gmail.com>
2022-05-21 01:02:18 -0500

    joke rendered, need to make punchline work

--- src/api/jokeData.js
+++ src/api/jokeData.js
@@ -3,9 +3,7 @@ import axios from 'axios';
 const endpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart';
 
 const getJoke = () => new Promise((resolve, reject) => {
-  axios.get(endpoint).then((response) => {
-    resolve(response.data);
-  }).catch((error) => reject(error));
+  axios.get(endpoint).then((response) => resolve(response.data)).catch((error) => reject(error));
 });
 
 export default getJoke;
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,21 @@ import '../styles/main.scss';
 import jokeData from '../api/jokeData';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
+  document.querySelector('#app').innerHTML = `<button id="joke-btn" type="button" class="joke-btn btn btn-primary" >Get a Joke</button><button id="punchLine" type="button" style='display: none' class="punch-btn btn btn-primary" >Punch Line ?</button>
   `;
 
   jokeData().then(console.warn);
 };
 
+const jokeEvent = () => {
+  document.querySelector('#joke-btn').addEventListener('click', () => {
+    jokeData().then((response) => {
+      document.querySelector('#jokeDiv').innerHTML = response.setup;
+      document.querySelector('#joke-btn').style.display = 'none';
+      document.querySelector('#punchLine').style.display = 'block';
+    });
+  });
+};
+
 init();
+jokeEvent();
